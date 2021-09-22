bb14107ec15770cd98ebe837fbb98bbf4eda4af9
maplemanju <kuzomegane@gmail.com>
2021-09-22 18:40:20 +0900

    style update

--- components/sidebar.js
+++ components/sidebar.js
@@ -2,25 +2,24 @@ import styles from './layout.module.scss'
 import utilStyles from '../styles/utils.module.scss'
 import Link from 'next/link'
 
+
 export default function Sidebar() {
   return(
     <aside className={styles.sidebar}>
       <div className={styles.sideContain}>
         <h2>Recent Posts</h2>
         <nav><ul>
-          <li>Sample</li>
-          <li>Sample</li>
-          <li>Sample</li>
-          <li>Sample</li>
+          <li className={styles.sideItem}>Sample</li>
+          <li className={styles.sideItem}>Sample</li>
+          <li className={styles.sideItem}>Sample</li>
         </ul></nav>
       </div>
       <div className={styles.sideContain}>
         <h2>Categories</h2>
         <nav><ul>
-          <li>Sample</li>
-          <li>Sample</li>
-          <li>Sample</li>
-          <li>Sample</li>
+          <li className={styles.sideItem}>Sample</li>
+          <li className={styles.sideItem}>Sample</li>
+          <li className={styles.sideItem}>Sample</li>
         </ul></nav>
       </div>
     </aside>
