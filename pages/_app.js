92da9576690a4827b369f9bf06b7e6ac4cf2ea24
maplemanju <kuzomegane@gmail.com>
2023-04-16 19:56:34 +0900

    add auto night mode detect

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,25 @@
 import '../styles/global.scss'
 import '../styles/prism.css'
 
+import { useEffect } from 'react'
+import moment from 'moment'
+
 export default function App({ Component, pageProps }) {
+  const detectTimeTheme = () => {
+    const currenTime = moment().hour()
+    let switchThisTheme = 'light'
+    if (currenTime >= 18) {
+      switchThisTheme = 'dark'
+    } else {
+      switchThisTheme = 'light'
+    }
+    console.log('auto theme', switchThisTheme)
+    document.documentElement.setAttribute('site-theme', switchThisTheme)
+  }
+
+  useEffect(() => {
+    detectTimeTheme()
+  }, [])
+
   return <Component {...pageProps} />
 }
